c42730f4533126ecf28b43ff62340266b28342b3
RorySpt <zhanglinlin689@outlook.com>
2023-07-10 19:03:17 +0800

    添加InputComponent

---
+++ src/sandbox/actor_input_component.cpp
@@ -0,0 +1,1 @@
+#include "actor_input_component.h"
---
+++ src/sandbox/actor_input_component.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string_view>
+
+#include "actor_component.h"
+
+enum EInputEvent : int
+{
+	IE_Pressed = 0,
+	IE_Released = 1,
+	IE_Repeat = 2,
+	IE_DoubleClick = 3,
+	IE_Axis = 4,
+	IE_MAX = 5,
+};
+
+class InputComponent :
+    public ActorComponent
+{
+	template<typename ResponseMethod>
+	void BindAction(std::string_view ActionName, const EInputEvent KeyEvent, Actor* ActorObject, ResponseMethod Func)
+	{
+	}
+
+	template<typename ResponseMethod>
+	void BindAction(std::string_view ActionName, Actor* ActorObject, ResponseMethod Func)
+	{
+	}
+};
+
