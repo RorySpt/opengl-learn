04c60f2d3cf0eb0fdc404764e2234f5006d3cb57
RorySpt <zhanglinlin689@outlook.com>
2023-09-22 10:17:39 +0800

    添加SphereModel，纯色材质的球体

---
+++ src/sandbox/SphereModel.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "common.h"
+
+#include "material.h"
+#include "IModel.h"
+#include "light.h"
+
+
+
+class SphereModel :IModel
+{
+public:
+	struct VertexInfo
+	{
+		glm::vec3 location;
+		glm::vec3 normal;
+	};
+	SphereModel();
+
+	void UpdateBufferData() const
+	{
+		glNamedBufferData(VBO, vertexes.size() * sizeof VertexInfo, vertexes.data(), GL_DYNAMIC_DRAW);
+		glNamedBufferData(EBO, indexes.size() * sizeof(GLuint), indexes.data(), GL_DYNAMIC_DRAW);
+	}
+	void initSphereVertexes(float radius, int segment = 10);
+
+	void draw(const Camera& camera, const glm::mat4& wMat) override
+	{
+		draw(camera, std::vector{ wMat });
+	}
+	void draw(const Camera& camera, const std::vector<glm::mat4>& wMats) override;
+
+	Material3 _material;
+	std::vector<LightSource> _lights;
+private:
+	GLuint EBO;
+	GLuint VBO;
+	GLuint VAO;
+
+	std::vector<VertexInfo> vertexes;
+	std::vector<GLuint> indexes;
+	std::shared_ptr<ShaderProgram> shader;
+};
+
